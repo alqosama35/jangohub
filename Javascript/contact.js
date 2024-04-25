71eec29d0926c9c5165e8d0343e6ed57a188472a
ismail <ismelmasry36@gmail.com>
2024-04-25 03:01:00 +0200

    removed unnecessary code

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -7,14 +7,4 @@ function displayInfo(name, code, job) {
                        "<strong>Job:</strong> " + job;
 
   infoContainer.classList.remove("hidden");
-}
-
-document.addEventListener("click", function(event) {
-  var infoContainer = document.getElementById("info-container");
-  var container = document.getElementById("content");
-
-  if (!infoContainer.contains(event.target)) {
-    infoContainer.classList.add("hidden");
-    container.classList.remove("centered");
-  }
-});
+};
