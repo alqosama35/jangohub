ee4f90517cbb2bb78e078691b214f2b1ded6ef07
ismail <ismelmasry36@gmail.com>
2024-04-30 23:06:37 +0300

    fixed the home page

--- JS/slide.js
+++ JS/slide.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const slide = document.getElementById('slide');
     const xmark = document.getElementById('list_xicon')
   
-    // Toggle the visibility of the container when the icon is clicked
+
     icon.addEventListener('click', function() {
       slide.classList.toggle('active');
         xmark.classList.toggle('active');
