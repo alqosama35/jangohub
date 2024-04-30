df2fa1316d05f1fe2a297a37f2b3d60a7cbc00b8
osama <alqosama35@gmail.com>
2024-05-01 00:05:20 +0300

    update href of menu

--- JS/results.js
+++ JS/results.js
@@ -1,8 +1,8 @@
 document.getElementById("theme").addEventListener("click", function() {
     var currentCSS = document.getElementById("css-link").getAttribute("href");
-    if (currentCSS === "CSS/resultsdark.css") {
-        document.getElementById("css-link").setAttribute("href", "CSS/socialmediad.css");
+    if (currentCSS === "CSS/results.css") {
+        document.getElementById("css-link").setAttribute("href", "CSS/resultsdark.css");
     } else {
-        document.getElementById("css-link").setAttribute("href", "CSS/socialmedia.css");
+        document.getElementById("css-link").setAttribute("href", "CSS/results.css");
     }
   });
